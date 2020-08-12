3dc1c2e38b4cc0a0f64af9ef770870d5ed884394
Abhinav Dhiman <abhinavg916@gmail.com>
2020-08-12 17:59:11 +0530

    Classname, Fragments, JSX, JS

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import "./App.css";
 
 // Functional Component
@@ -16,12 +16,53 @@ import "./App.css";
 class App extends Component {
   // Imported at the top
   render() {
-    // Lifecycle Method - Runs at certain points to render components
-    return (
-      <div className="App">
-        <h1>Hello from React</h1>
-      </div>
-    );
+    // render() is one of the lifecycle method which runs at certain points to render components
+    
+    // Method 1: Using JSX 
+    // return (
+    
+      // className
+      // <div className="App">
+      //     <h1>Hello from React</h1>
+      //     {/* JSX - JavaScript Syntax Extension, basically syntatic sugar to be able to write the output of the components in XML or HTML like way but under the hood it is JavaScript.
+      //         JSX saves time as less code to write for output*/}
+      //     {/* Diff b/n HTML & JSX: HTML - class and JSX - className; HTML - for and JSX - htmlFor */}
+      //     {/* All JSX element must be have atleast one parent element otherwise use Fragment toa avoid */}
+      //     <h2>Good bye</h2>    // Works because div is the parent element
+      //     index.html using className
+      //     <div id="root">
+      //     <div class="App">
+      //     <h1>Hello from React</h1>
+      //     <h2>Good bye</h2>
+      //     </div>
+      //     </div>
+      //  </div>
+      //     <h2>Good bye</h2>    // Doesn't work because it has no parent element
+
+      // Fragments
+      // Method 1 - Nornal Way
+        // <Fragment>
+        //   <h1>Hello from React</h1>
+        //   <h2>Good bye</h2> {/* Works without any parent element */}
+        // </Fragment>
+        // index.html using Fragments
+        // <div id="root">
+        //   <h1>Hello from React</h1>
+        //   <h2>Good bye</h2>
+        // </div>
+        // 
+      // Method 2 - Another way of declaring fragment
+      // <>
+      // <h1>Hello from React</h1>
+      // <h2>Good bye</h2> {/* Works without any parent element */}
+      // </>
+
+    // );
+
+    // Method 2: Using JavaScript
+      return React.createElement('div', {className: 'App'}, React.createElement('h1',null, 'Hello from React'));
+      // JavaScript way of writing can get code very messy and hard to understand.abs
+      // That's why, JSX is preferred in ReactJs
   }
 }
 export default App;

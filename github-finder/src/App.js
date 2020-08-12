d3ada5996cde772a7e53455416c2018bc6c2b7a6
Abhinav Dhiman <abhinavg916@gmail.com>
2020-08-12 17:27:32 +0530

    Class Components

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Hello from React</h1>
-    </div>
-  );
+// Functional Component
+// function App() {
+//   return (
+//     <div className="App">
+//      <h1>Hello from React</h1>
+//     </div>
+//   );
+// }
+
+// Class Compenents
+// class App extends React.Component {
+// React.Component class contains all the lifecycle methods
+class App extends Component {
+  // Imported at the top
+  render() {
+    // Lifecycle Method - Runs at certain points to render components
+    return (
+      <div className="App">
+        <h1>Hello from React</h1>
+      </div>
+    );
+  }
 }
-
 export default App;
